c541ea6609728b6e9a9a874219209c8e9ffd15d7
CiksGg <kambarbekovamantur81@gmail.com>
2022-12-19 11:58:56 +0600

    account views

--- account/views.py
+++ account/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from django.shortcuts import get_object_or_404
+from drf_yasg.utils import swagger_auto_schema
+from .serializers import RegisterUserSerializer
+from .models  import User
+
 # Create your views here.
+
+class RegisterUserView(APIView):
+    @swagger_auto_schema(request_body=RegisterUserSerializer())
+    def post(self, request):
+        serializer = RegisterUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response('Вы успешно зарегистрировались', status=201)
+
+
+class DeleteUserView(APIView):
+    def delete(self, request,email):
+        user = get_object_or_404(User,email=email)
+        print(user)
+        print(request.user)
+        if user.is_staff or user != request.user:
+            return Response(status=403)
+        user.delete()
+        return Response('Успешно удалено',status=204)
