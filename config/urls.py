99ed33cf43cbd6f10a94bf5ff210519977ee84e0
adelia <adelia.ad03@gmail.com>
2022-12-19 12:54:28 +0600

    main model + settings media

--- config/urls.py
+++ config/urls.py
@@ -33,3 +33,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', schema_view.with_ui("swagger")),
 ]
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Film(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    year = models.IntegerField()
+    genre = models.CharField(max_length=20, choices=[('фантастика', 'fantasy'), ('боевик', 'thriller'), ('детектив', 'detective'), ('комедия', 'comedy'), ('мелодрама', 'melodrama'), ('научный', 'science'), ('приключения', 'adventures'), ('ужасы', 'horror'), ('спорт', 'sport'), ('мультфильм', 'cartoon'), ('документальный', 'documentary')])
+    producer = models.CharField(max_length=70)
+    actors = models.TextField()
+    duration = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='films')
+    video = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
