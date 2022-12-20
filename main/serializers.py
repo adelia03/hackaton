d32ec9eff9ce0fc1ce291a5c85425108b7f544ac
adelia <adelia.ad03@gmail.com>
2022-12-20 14:49:47 +0600

    permissions

--- main/serializers.py
+++ main/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework.serializers import ModelSerializer
+
 from .models import Film
+from review.serializers import *
 
 class FilmSerializer(ModelSerializer):
     class Meta:
         model = Film
         fields = '__all__'
+   
--- main/views.py
+++ main/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import get_object_or_404
 from rest_framework.viewsets import ModelViewSet
-
-
 
 from .models import Film
 from .serializers import FilmSerializer
@@ -9,3 +6,4 @@ from .serializers import FilmSerializer
 class FilmViewSet(ModelViewSet):
     serializer_class = FilmSerializer
     queryset = Film.objects.all()
+    
