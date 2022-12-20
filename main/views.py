0c4473fb0943a1858a0c354ba0d21c28cccb989b
CiksGg <kambarbekovamantur81@gmail.com>
2022-12-20 11:57:32 +0600

    v1

--- main/views.py
+++ main/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.viewsets import ModelViewSet
+from .filters import FilmFilter
 # from rest_framework.response import Response
+
+from rest_framework.decorators import action
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from django.db.models import Q
+from rest_framework.response import Response
+
 
 
 from .models import Film
@@ -9,3 +17,24 @@ from .serializers import FilmSerializer
 class FilmViewSet(ModelViewSet):
     serializer_class = FilmSerializer
     queryset = Film.objects.all()
+    filterset_class = FilmFilter
+
+
+    @swagger_auto_schema(manual_parameters=[
+        openapi.Parameter('q',openapi.IN_QUERY, type=openapi.TYPE_STRING)
+    ])
+    @action(['GET'], detail=False)
+    def search(self,requests):
+        q = requests.query_params.get('q')
+        queryset = self.get_queryset()
+        if q:
+            queryset = queryset.filter(Q(title_icontains=q) | Q(body_icontains=q))
+        pagination = self.paginate_queryset(queryset)
+        if pagination:
+            serializers = self.get_serializer(pagination, many=True)
+            return self.get_paginated_response(serializers.data)
+        serializers = self.get_serializer(queryset, many=True)
+        return Response(serializers.data, status=201)
+
+
+
