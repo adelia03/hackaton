2ada7d53dccddfc273b6c94ef588dee67234ddc4
Aiup <64644481+Ayup923@users.noreply.github.com>
2022-12-23 11:53:07 +0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'deploy-production-59e4.up.railway.app']
 
